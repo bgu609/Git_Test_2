7e110ba0472c034518cdb3410d8c295f686694bf
bgw <giung609@naver.com>
2022-04-26 15:50:17 +0900

    TEST2

---
+++ TEST2/Test_2.1/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Test_Util_1;
+
+namespace Test_2._1
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Test_Util_1_Source Test_Util_1_Source;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Test_Util_1_Source = new Test_Util_1_Source();
+
+            View_Text.Text = Test_Util_1_Source.Instance_Name;
+        }
+    }
+}
