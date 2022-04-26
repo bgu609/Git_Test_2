bb01278918a62dd73363cee9dc96d8262b0d197f
bgw <giung609@naver.com>
2022-04-26 16:27:53 +0900

    share project test

--- TEST2/Test_2.1/MainWindow.xaml.cs
+++ TEST2/Test_2.1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Test_ShareProject_2;
 using Test_Util_1;
 
 namespace Test_2._1
@@ -9,14 +10,17 @@ namespace Test_2._1
     public partial class MainWindow : Window
     {
         Test_Util_1_Source Test_Util_1_Source;
+        Test_Share_2_Source Test_Share_2_Source;
 
         public MainWindow()
         {
             InitializeComponent();
 
             Test_Util_1_Source = new Test_Util_1_Source();
+            Test_Share_2_Source = new Test_Share_2_Source();
 
             View_Text.Text = Test_Util_1_Source.Instance_Name;
+            View_ShareText.Text = Test_Share_2_Source.Sharing_Name;
         }
     }
 }
---
+++ TEST2/Test_ShareProject_2/Test_Share_2_Source.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_ShareProject_2
+{
+    public class Test_Share_2_Source
+    {
+        public string Sharing_Name = "Sharing 2";
+    }
+}
